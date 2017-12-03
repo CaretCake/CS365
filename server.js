e8c38dd280883f213b59a21c7162b6688dc76246
kguidebeck <33331990+kguidebeck@users.noreply.github.com>
2017-12-03 14:37:12 -0500

    Update server.js
    kimber 12/3 changes

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ var io = socketIo(server);
 
 var db;
 
-var guessedPlayers = [];
 var players = [];
+var gameScores = [];
+var roundScores = [];
 var allSockets = [];
 var words = ["football", "needle", "swing", "flower", "cookie", "ghost", "jellyfish", "lollipop", "hockey", "treasure"];
-var setWord = " ";
+var setWord = "";
+var votekickCount = 0;
 
 function randomElementIn(theArray) {
 	var i = Math.floor(theArray.length * Math.random());
@@ -44,59 +46,16 @@ app.use(express.static("pub"));
 
 io.on("connection", function(socket) {
 	console.log("Connection made");
+	var loggedIn = false;
 
 	socket.on("disconnect", function() {
 		console.log("disconnection made");
-		var k = players.indexOf(socket);
-		allSockets.splice(k,1);
-		players.splice(k,1);
-		io.emit("updateUsers", players);
-	});
-
-	socket.on("chat", function(textInput) {
-		var i = allSockets.indexOf(socket);
-		for(var j = 0; j < guessedPlayers.length; j++){
-			if(guessedPlayers[j] == players[i])
-				var playerAlreadyGuessed = true;
-			else
-				playerAlreadyGuessed = false;
+		if(loggedIn){
+			var k = players.indexOf(socket);
+			allSockets.splice(k,1);
+			players.splice(k,1);
+			io.emit("updateUsers", players);
 		}
-		if(playerAlreadyGuessed){
-			var textString = "**" + players[i].name + "**: " + textInput;
-			io.emit("sayAll", textString);
-		}
-		else{
-			console.log(setWord);
-			if(textInput.toLowerCase() === setWord.toLowerCase()){
-				players[i].guessed = true;
-				guessedPlayers.push(players[i]);
-				var textString = "****" + players[i].name + " has guessed the word!****";
-				io.emit("sayAll", textString);
-				io.emit("updateUsers", players);
-			}
-			else{
-				var textString = players[i].name + ": " + textInput;
-				io.emit("sayAll", textString);
-			}
-		}
-	});
-
-	socket.on("getThreeWords", function(){
-		var word1 = randomElementIn(words);
-		var word2 = randomElementIn(words);
-		while(word1 == word2)
-			word2 = randomElementIn(words);
-		var word3 = randomElementIn(words);
-		while(word3 == word1 || word3 == word2)
-			word3 = randomElementIn(words);
-		socket.emit("displayWords", word1, word2, word3);
-	});
-
-	socket.on("getChosenWord", function(chosenWord){
-		console.log(chosenWord);
-		setWord = chosenWord;
-		// TODO: Change when no longer necessary
-		io.emit("displayWordToAll", chosenWord);
 	});
 
 	socket.on("login", function(username) {
@@ -115,13 +74,98 @@ io.on("connection", function(socket) {
 			}
 		}
 		allSockets.push(socket);
+		loggedIn = true;
 		var socketNum = allSockets.indexOf(socket);
-		if(allSockets.length == 1)
-			players.push({name: username, score: 0, drawer: true, guessed: false, rank: allSockets.length, answerRank: 0, socket: socketNum});
-		else
-			players.push({name: username, score: 0, drawer: false, guessed: false, rank: allSockets.length, answerRank: 0, socket: socketNum});
+		var sessionid = socket.id;
+		if(allSockets.length == 1){
+			players.push({name: username, score: 0, roundScore: 0, drawer: true, guessed: false, rank: allSockets.length, roundRank: 0, socketID: sessionid});
+			socket.join('drawer');
+			socket.join('guessedWord');
+		}
+		else{
+			players.push({name: username, score: 0, roundScore: 0, drawer: false, guessed: false, rank: allSockets.length, roundRank: 0, socketID: sessionid});
+			socket.join('guessers');
+		}
 		socket.emit("loginOk");
 		io.emit("updateUsers", players);
+	});
+
+	socket.on("chat", function(textInput) {
+		var i = allSockets.indexOf(socket);
+		if(players[i].guessed == true || players[i].drawer == true){
+			var textString = "**" + players[i].name + "**: " + textInput;
+			io.in('guessedWord').emit("sayAll", textString);
+		}
+		else{
+			if(textInput.toLowerCase() === setWord.toLowerCase()){
+				players[i].guessed = true;
+				roundScores.push({name: players[i].name, score: 5*30});
+				socket.join('guessedWord');
+				var textString = "****" + players[i].name + " has guessed the word!****";
+				io.emit("sayAll", textString);
+				io.emit("updateUsers", players);
+			}
+			else{
+				var textString = players[i].name + ": " + textInput;
+				io.emit("sayAll", textString);
+			}
+		}
+	});
+
+	socket.on("votekick", function(){
+		for(var i = 0; i < players.length; i++){
+			if(players[i].drawer == true)
+			 var drawerIndex = i;
+		}
+		var kickerIndex = allSockets.indexOf(socket);
+		votekickCount++;
+		if(players.length % 2 == 0){
+			if(players.length == 2){
+				var textString = "";
+				if(votekickCount == 1)
+					var votekick = true;
+			}
+			else{
+				var textString = "A player has voted to kick the drawer (" + votekickCount + " of " + (players.length/2 + 1) + ")";
+				if(votekickCount == (players.length/2 + 1))
+					var votekick = true;
+			}
+		}
+		else{
+			var textString = "A player has voted to kick the drawer (" + votekickCount + " of " + ((players.length + 1)/2) + ")";
+			if(votekickCount == ((players.length + 1)/2))
+				var votekick = true;
+		}
+		io.emit("sayAll", textString);
+		if(votekick){
+			var errorMessage = "Sorry you've been kicked from the game.";
+			let socket = io.sockets.connected[players[drawerIndex].socketID];
+			socket.emit('playerKicked', errorMessage);
+			socket.leave('drawer');
+			allSockets.splice(drawerIndex,1);
+			players.splice(drawerIndex,1);
+			io.emit("updateUsers", players);
+		}
+	});
+
+	socket.on("getThreeWords", function(){
+		var word1 = randomElementIn(words);
+		var word2 = randomElementIn(words);
+		while(word1 == word2)
+			word2 = randomElementIn(words);
+		var word3 = randomElementIn(words);
+		while(word3 == word1 || word3 == word2)
+			word3 = randomElementIn(words);
+		socket.emit("displayWords", word1, word2, word3);
+	});
+
+	socket.on("getChosenWord", function(chosenWord){
+		console.log(chosenWord);
+		setWord = chosenWord;
+		// TODO: Change when no longer necessary
+		socket.emit("displayWordToDrawer", chosenWord);
+		socket.broadcast.to('guessers').emit("displayWordToAll", chosenWord);
+
 	});
 
 
